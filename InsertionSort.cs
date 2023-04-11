2ab33dcd2135a7ae9d0b4b7ad2371ac6a4ff3ee5
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 23:35:03 +0530

    Insertion sorting

---
+++ InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchAndSorting
+{
+    public class InsertionSort
+    {
+        public void DisplayInsertion()
+        {
+            string[] arr = new string[] { "Bhole", "Ram", "Shyam", "Durga" };
+            Console.WriteLine("unsorted Array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            for (int i = 1; i < arr.Length; i++)
+            {
+                string value = arr[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[j].CompareTo(value) > 0)
+                    {
+                        string temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(" Sorted Array");
+            foreach (string item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+}
