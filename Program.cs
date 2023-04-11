6756f7a5e070f67e99f37e8f329e0c0f6aa5f945
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-12 00:10:25 +0530

    check anagram

--- Program.cs
+++ Program.cs
@@ -6,12 +6,8 @@
         {
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
-
-            FindNumber obj = new FindNumber();
-            Console.WriteLine("Enter the limit:");
-            int N = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Guess value Between 0 to {N}");
-            obj.call(0, N - 1);
+            //ANAGRAM
+            Anagram.CheckAnagram();
         }
     }
 }
