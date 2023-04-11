feffc0e699ca93b039cbfc7d2ba137f29055e9c7
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 23:46:35 +0530

    prime number

--- Program.cs
+++ Program.cs
@@ -7,21 +7,9 @@
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
 
-            List<string> list = new List<string> { "Ram", "Bhole", "Shiv", "Khanha", "Durga", "Lakshmi" };
-            Console.WriteLine("Unsorted List:");
-            foreach (string s in list)
-            {
-                Console.Write(s + " ");
-            }
-            Console.WriteLine();
-
-            List<string> sorted = MergeSorting.Sort(list);
-            Console.WriteLine("Sorted List:");
-            foreach (string s in sorted)
-            {
-                Console.Write(s + " ");
-            }
-            Console.WriteLine();
+            //PRIME NUMBER IN RANGE
+            PrimeNumber obj = new PrimeNumber();
+            obj.prime();
         }
     }
 }
