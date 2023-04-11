368af7a4cd514217e1442a8a78ffe9c3e01066d3
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-12 00:00:54 +0530

    find number

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
 
-            //PRIME NUMBER IN RANGE
-            PrimeNumber obj = new PrimeNumber();
-            obj.prime();
+            FindNumber obj = new FindNumber();
+            Console.WriteLine("Enter the limit:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Guess value Between 0 to {N}");
+            obj.call(0, N - 1);
         }
     }
 }
