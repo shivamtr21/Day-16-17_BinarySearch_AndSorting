56a022836766c835fde1bef5d8550a30a4f433f3
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 23:42:15 +0530

    Marge sorting

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Binary search and Sorting");
-            InsertionSort insertionSort = new InsertionSort();
-            insertionSort.DisplayInsertion();
+            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
+            Console.WriteLine("*********************");
+
+            List<string> list = new List<string> { "Ram", "Bhole", "Shiv", "Khanha", "Durga", "Lakshmi" };
+            Console.WriteLine("Unsorted List:");
+            foreach (string s in list)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            List<string> sorted = MergeSorting.Sort(list);
+            Console.WriteLine("Sorted List:");
+            foreach (string s in sorted)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
