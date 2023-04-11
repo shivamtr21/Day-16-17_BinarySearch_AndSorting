3a43b7209d45e0159b4110155db87d8721ba8c65
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 23:35:50 +0530

    Insertion sorting

--- Program.cs
+++ Program.cs
@@ -5,9 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Binary search and Sorting");
-
-            BubbleSort shiv = new BubbleSort();
-            shiv.DisplayBubble();
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.DisplayInsertion();
         }
     }
 }
